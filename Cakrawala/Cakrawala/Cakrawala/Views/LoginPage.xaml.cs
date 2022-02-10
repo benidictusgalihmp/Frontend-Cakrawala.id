cabd3100ab786a35d15c3536e1d5bd52db57b420
imulki <64567731+imulki@users.noreply.github.com>
2022-02-10 16:03:37 +0700

    Create Login Page

---
+++ Cakrawala/Cakrawala/Cakrawala/Views/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cakrawala.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
