885220b895eaf708a6e648f9d0b51e8ff2c2467a
imulki <64567731+imulki@users.noreply.github.com>
2022-02-11 02:11:13 +0700

    Add Register and Dashboard page

---
+++ Cakrawala/Cakrawala/Cakrawala/Views/DashboardPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cakrawala.Views
+{
+    [QueryProperty(nameof(Email), "email")]
+    public partial class DashboardPage : ContentPage
+    {
+        public string Email
+        {
+            set
+            {
+                OnChangeEmail(value);
+            }
+            get { return Email; }
+        }
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnChangeEmail(string value) 
+        {
+            MainLabel.Text = $"Selamat Datang, {value.ToString()}";
+            BindingContext = value;
+        }
+    }
+}
