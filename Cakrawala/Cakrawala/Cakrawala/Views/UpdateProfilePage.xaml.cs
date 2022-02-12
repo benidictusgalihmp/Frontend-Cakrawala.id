1cddfa1bc8cd1722c413ad34e2abd2c20a1ed33c
benidictusgalihmp <68523239+benidictusgalihmp@users.noreply.github.com>
2022-02-12 15:49:42 +0700

    [13519159] feat: add update profile view

---
+++ Cakrawala/Cakrawala/Cakrawala/Views/UpdateProfilePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cakrawala.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UpdateProfilePage : ContentPage
+    {
+        public UpdateProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void onClick(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//home");
+
+        }
+    }
+}
