f792470b7fc111b41acaf42999468e9a44cb1dce
imulki <64567731+imulki@users.noreply.github.com>
2022-03-16 19:16:02 +0700

    create Constants class

---
+++ Cakrawala/Cakrawala/Cakrawala/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cakrawala
+{
+    public static class Constants
+    {
+        public static string RestUrl = "http://cakrawalaidapi.southeastasia.cloudapp.azure.com/";
+    }
+}
