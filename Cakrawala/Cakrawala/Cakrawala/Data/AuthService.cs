6edf20842f16eb90f0a10983f19b86e7a267b15b
imulki <64567731+imulki@users.noreply.github.com>
2022-03-17 11:51:58 +0700

    use Newtonsoft.JSON for json

--- Cakrawala/Cakrawala/Cakrawala/Data/AuthService.cs
+++ Cakrawala/Cakrawala/Cakrawala/Data/AuthService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -12,25 +12,28 @@ namespace Cakrawala.Data
     public class AuthService
     {
         HttpClient client;
-        JsonSerializerOptions serializerOptions;
+        //JsonSerializerOptions serializerOptions;
 
         public AuthService() 
         {
             client = new HttpClient();
+            /*
             serializerOptions = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true,
             };
+            */
             
         }
 
-        public async Task RegisterAsync(string username, string email, string password)
+        public async Task<bool> RegisterAsync(string username, string name, string email, string password)
         {
             Uri uri = new Uri(string.Format(Constants.RestUrl+"users/register", string.Empty));
+            bool output = false;
             try
             {
-                string json = JsonSerializer.Serialize<RegisterFormat>(new RegisterFormat(username, email, password), serializerOptions);
+                string json = JsonConvert.SerializeObject(new RegisterFormat(username, name, email, password));
                 StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
                 HttpResponseMessage response = await client.PostAsync(uri, content);
@@ -38,12 +41,17 @@ namespace Cakrawala.Data
                 if (response.IsSuccessStatusCode)
                 {
                     Console.WriteLine("Success Register");
+                    output = true;
                 }
+
+                Debug.WriteLine(response.ToString());
             }
             catch (Exception ex)
             {
                 Console.WriteLine(@"\tERROR {0}", ex.Message);
             }
+
+            return output;
         }
 
         public async Task<LoginResponse> LoginAsync(string email, string password)
@@ -58,7 +66,7 @@ namespace Cakrawala.Data
                 LoginFormat item = new LoginFormat(email, password);
 
                 Debug.WriteLine("Start Make kontent");
-                string json = JsonSerializer.Serialize<LoginFormat>(item, serializerOptions);
+                string json = JsonConvert.SerializeObject(item);
                 StringContent content = new StringContent(json, Encoding.UTF8 , "application/json");
 
                 Debug.WriteLine("Start Send Post Asnyc");
@@ -72,7 +80,7 @@ namespace Cakrawala.Data
                 if (response.IsSuccessStatusCode)
                 {
                     string rawLogResp = await response.Content.ReadAsStringAsync();
-                    logResp = JsonSerializer.Deserialize<LoginResponse>(rawLogResp, serializerOptions);
+                    logResp = JsonConvert.DeserializeObject<LoginResponse>(rawLogResp);
 
                     Debug.WriteLine(logResp.username);
                     Debug.WriteLine("Login Successful");
@@ -81,6 +89,7 @@ namespace Cakrawala.Data
             catch(Exception ex)
             {
                 Debug.WriteLine("Error" + ex.Message);
+                logResp.jwtToken = "error";
             }
 
             return logResp;
@@ -90,7 +99,10 @@ namespace Cakrawala.Data
 
         private class LoginFormat
         {
+            [JsonProperty(PropertyName = "email")]
             public string email { get; set; }
+
+            [JsonProperty(PropertyName = "password")]
             public string password { get; set; }
 
             public LoginFormat(string email, string password)
@@ -102,17 +114,24 @@ namespace Cakrawala.Data
 
         public class LoginResponse
         {
+            [JsonProperty(PropertyName = "id")]
             public string id { get; set; }
+
+            [JsonProperty(PropertyName = "username")]
             public string username { get; set; }
+
+            [JsonProperty(PropertyName = "email")]
             public string email { get; set; }
-            public string jwt_token { get; set; }
+
+            [JsonProperty(PropertyName = "jwt_token")]
+            public string jwtToken { get; set; }
 
             public LoginResponse()
             {
                 this.id = null;
                 this.username = null;
                 this.email = null;
-                this.jwt_token = null;
+                this.jwtToken = null;
             }
 
             public LoginResponse(string id, string username, string email, string jwtToken)
@@ -120,19 +139,28 @@ namespace Cakrawala.Data
                 this.id = id;
                 this.username = username;
                 this.email=email;
-                this.jwt_token = jwtToken;
+                this.jwtToken = jwtToken;
             }
         }
 
         private class RegisterFormat
         {
-            public string username;
-            public string email;
-            public string password;
+            [JsonProperty(PropertyName = "username")]
+            public string username { get; set; }
 
-            public RegisterFormat(string username, string email, string password)
+            [JsonProperty(PropertyName = "name")]
+            public string name { get; set; }
+
+            [JsonProperty(PropertyName = "email")]
+            public string email { get; set; }
+
+            [JsonProperty(PropertyName = "password")]
+            public string password { get; set; }
+
+            public RegisterFormat(string username, string name, string email, string password)
             {
                 this.username = username;
+                this.name = name;
                 this.email = email;
                 this.password = password;
             }
