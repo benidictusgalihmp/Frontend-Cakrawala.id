bc75dc85bd36af751db34bab9e1ea4122ac8750e
benidictusgalihmp <68523239+benidictusgalihmp@users.noreply.github.com>
2022-04-16 13:44:36 +0700

    feat: integrate update profile

--- Cakrawala/Cakrawala/Cakrawala/Data/ProfileService.cs
+++ Cakrawala/Cakrawala/Cakrawala/Data/ProfileService.cs
@@ -44,5 +44,93 @@ namespace Cakrawala.Data
 
             return viewProfileResp;
         }
+
+        public async Task<bool> UpdateUsernameAsync(
+            string userId,
+            string newUsername)
+        {
+            Uri uri = new Uri(string.Format(Constants.RestUrl + "users/" + userId, string.Empty));
+            Task<User> user = ViewProfileAsync(userId);
+            bool output = false;
+
+            String oldUpdatePassword = user.password;
+            String newUpdatePassword = oldUpdatePassword;
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(new UpdateProfileFormat(newUsername, newUpdatePassword, oldUpdatePassword));
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Success Change Username");
+                    output = true;
+                }
+
+                Debug.WriteLine(response.ToString());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error" + ex.Message);
+            }
+            return output;
+        }
+
+        public async Task<bool> UpdatePasswordAsync(
+            string userId,
+            string newUpdatePassword)
+        {
+            Uri uri = new Uri(string.Format(Constants.RestUrl + "users/" + userId, string.Empty));
+            Task<User> user = ViewProfileAsync(userId);
+            bool output = false;
+
+            String newUsername = user.userName;
+            String oldUpdatePassword = user.password;
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(new UpdateProfileFormat(newUsername, newUpdatePassword, oldUpdatePassword));
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Success Change Password");
+                    output = true;
+                }
+
+                Debug.WriteLine(response.ToString());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error" + ex.Message);
+            }
+            return output;
+        }
+
+        private class UpdateProfileFormat
+        {
+            [JsonProperty(PropertyName = "newUsername")]
+            public string newUsername { get; set; }
+
+            [JsonProperty(PropertyName = "newUpdatePassword")]
+            public string newUpdatePassword { get; set; }
+
+            [JsonProperty(PropertyName = "oldUpdatePassword")]
+            public string oldUpdatePassword { get; set; }
+
+            public UpdateProfileFormat(
+                string newUsername, 
+                string newUpdatePassword, 
+                string oldUpdatePassword)
+            {
+                this.newUsername = newUsername;
+                this.newUpdatePassword = newUpdatePassword;
+                this.oldUpdatePassword = oldUpdatePassword;
+            }
+        }
     }
 }
